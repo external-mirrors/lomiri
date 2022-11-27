ubports/unity8

qml: Re-introduce interactive Drawer blur on capable devices
On device ports which set the necessary getprop property, allow the
use of interactive blur in the drawer. Use static blur otherwise.

qml/OrientedShell.qml
@@ -273,6 +273,7 @@ Item {
         nativeWidth: root.width
         nativeHeight: root.height
         mode: applicationArguments.mode
+        interactiveBlur: applicationArguments.interactiveBlur
         hasMouse: pointerInputDevices > 0
         hasKeyboard: keyboardsModel.count > 0
         hasTouchscreen: touchScreensModel.count > 0

external-mirrors/lomiri

qml/OrientedShell.qml
@@ -273,6 +273,7 @@ Item {
         nativeWidth: root.width
         nativeHeight: root.height
         mode: applicationArguments.mode
+        interactiveBlur: deviceConfiguration.interactiveBlur
         hasMouse: pointerInputDevices > 0
         hasKeyboard: keyboardsModel.count > 0
         hasTouchscreen: touchScreensModel.count > 0