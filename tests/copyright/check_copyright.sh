4f2e83c458b7dbf23b65ae514d9bbc7694452be4
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2024-04-22 09:08:20 +0200

    tests/copyright/check_copyright.sh: Ignore quilt files in .pc/ subfolder.

--- tests/copyright/check_copyright.sh
+++ tests/copyright/check_copyright.sh
@@ -34,7 +34,7 @@ usage()
 # image files
 ignore_pat="\\.sci$|\\.svg$|\\.png$|\\.jpg$"
 # git/bzr files
-ignore_pat="$ignore_pat|/\\.bzr/|/\\.git/|\\.bzrignore$|\\.gitignore$|/\\.bazaar/Makefile$|/\\.bzr-builddeb/default\\.conf$"
+ignore_pat="$ignore_pat|/\\.pc|/\\.bzr/|/\\.git/|\\.bzrignore$|\\.gitignore$|/\\.bazaar/Makefile$|/\\.bzr-builddeb/default\\.conf$"
 # info files
 ignore_pat="$ignore_pat|/AUTHORS$|/README(.md)?$|/CODING$|/ChangeLog$|/LGPL_EXCEPTION\\.txt$"
 # cmake files
