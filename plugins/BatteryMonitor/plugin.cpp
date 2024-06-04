ccfab3adbc3b47e7deae7d0843428232119b4c1a
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2024-06-04 06:08:51 +0200

    plugins/BatteryMonitor/plugin.cpp: Amend copyright holdership.
    As discussed in https://gitlab.com/ubports/development/core/lomiri/-/merge_requests/142
    
    Follow-up for b83737931e0d5767939a3b9b62ff2e2e209df36e.

--- plugins/BatteryMonitor/plugin.cpp
+++ plugins/BatteryMonitor/plugin.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 UBports Foundation
+ * Copyright (C) 2023 Muhammad <muhammad23012009@hotmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
