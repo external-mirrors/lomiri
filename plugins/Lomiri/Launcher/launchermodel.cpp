external-mirrors/lomiri

commit 1430cd6df2358f7285e98a3639f9b6ca0a8e89ef

plugins/Lomiri/Launcher: Plug a memory leak

plugins/Lomiri/Launcher/launchermodel.cpp
@@ -561,10 +561,12 @@ void LauncherModel::updateSurfaceListForSurface()
 
 void LauncherModel::updateSurfaceListForApp(ApplicationInfoInterface* app)
 {
+    LauncherItem *item = nullptr;
     int idx = findApplication(app->appId());
+
     if (idx < 0) {
         qWarning() << "Received a surface count changed event from an app that's not in the Launcher model, creating icon...";
-        LauncherItem *item = new LauncherItem(app->appId(), app->name(), app->icon().toString(), this);
+        item = new LauncherItem(app->appId(), app->name(), app->icon().toString(), this);
         item->setRecent(true);
         item->setRunning(true);
         item->setFocused(app->focused());
@@ -576,10 +578,12 @@ void LauncherModel::updateSurfaceListForApp(ApplicationInfoInterface* app)
 
     if (idx < 0) {
         qWarning() << "Couldn't create launcher icon.";
+        if (item)
+            delete item;
         return;
     }
 
-    LauncherItem *item = m_list.at(idx);
+    item = m_list.at(idx);
     if (!item->pinned() && item->running() && app->surfaceCount() <= 0) {
         beginRemoveRows(QModelIndex(), idx, idx);
         m_list.takeAt(idx)->deleteLater();

commit dc1a8671573f3f739389152a2c83edc1adfa61f3

plugins/Lomiri/Launcher: remove "leaked" item from list before deleting
Since the "leaked" item is already added to the list, one can't simply
delete it. It has to be removed from the list first, or it will be a
use-after-free.

Actually, it's not clear under what situation will the item be added but
not returned from `findApplication()` (as ASAdapter doesn't seem to
modify the list). But I'll let that code stay...

Fixes: 1430cd6d ("plugins/Lomiri/Launcher: Plug a memory leak")

plugins/Lomiri/Launcher/launchermodel.cpp
@@ -583,8 +583,13 @@ void LauncherModel::updateSurfaceListForApp(ApplicationInfoInterface* app)
     // Only delete if we're sure it's not in the list.
     if (idx < 0) {
         qWarning() << "Couldn't create launcher icon.";
-        if (item)
+        if (item) {
+            beginRemoveRows(QModelIndex(), m_list.count() - 1, m_list.count() - 1);
+            m_list.removeLast();
+            endRemoveRows();
+
             delete item;
+        }
         return;
     }
 