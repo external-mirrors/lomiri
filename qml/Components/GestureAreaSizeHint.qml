external-mirrors/lomiri

commit 08d48e53f6459d57de84a1a977682777f5d3a626

Add visual hint when left and right edge gesture areas are resized

qml/Components/GestureAreaSizeHint.qml
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2025 UBports Foundation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import QtQuick 2.12
+import Lomiri.Components 1.3
+
+Rectangle {
+    id: root
+
+    function show() {
+        opacity = 0.3
+        timeoutTimer.restart()
+    }
+
+    enabled: false
+    opacity: 0
+    visible: opacity > 0
+    color: theme.palette.normal.activity
+
+    Behavior on opacity { LomiriNumberAnimation {} }
+
+    Timer {
+        id: timeoutTimer
+        interval: 2000
+        onTriggered: root.opacity = 0
+    }
+
+    Connections {
+        target: root.parent
+        enabled: root.enabled
+
+        // Avoid showing up when Lomiri starts
+        Component.onCompleted: root.enabled = true
+
+        onWidthChanged: root.show()
+        onHeightChanged: root.show()
+    }
+}

qml/Launcher/Launcher.qml
@@ -610,6 +610,10 @@ FocusScope {
             }
             lastDragPoints = [];
         }
+
+        GestureAreaSizeHint {
+            anchors.fill: parent
+        }
     }
 
     states: [

qml/Stage/Stage.qml
@@ -2380,6 +2380,10 @@ FocusScope {
                 }
             }
         }
+
+        GestureAreaSizeHint {
+            anchors.fill: parent
+        }
     }
 
     TabletSideStageTouchGesture {

commit c218689eb7148dbab35e4d97b89ca89596805a68

GestureAreaSizeHint: don't display on height change, including on rotate
Despite the description of MR 201 [1] says, the visual hint will also be
displayed when height is changed, not only width. This is problematic;
the height of the area will be changed when the shell is rotated,
resulting in the hint being displayed in a seemingly unrelated
situation.

So just remove the handler for height change. The usecase of this seems
to be to display visually when the width is adjusted in the settings;
this commit should not affect that.

Fixes: 08d48e53 ("Add visual hint when left and right edge gesture areas are resized")

[1]: https://gitlab.com/ubports/development/core/lomiri/-/merge_requests/201

qml/Components/GestureAreaSizeHint.qml
@@ -46,6 +46,5 @@ Rectangle {
         Component.onCompleted: root.enabled = true
 
         onWidthChanged: root.show()
-        onHeightChanged: root.show()
     }
 }