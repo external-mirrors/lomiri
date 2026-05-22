external-mirrors/lomiri

commit 463493b237a09afdc18a55a40e34b9ffe98e26b3

Shell: Add USFPS dimmer

qml/Shell.qml
@@ -264,6 +264,11 @@ StyledItem {
         id: volumeControl
     }
 
+    UDFPSDimmer{
+        anchors.fill: parent
+        z: 9
+    }
+
     PhysicalKeysMapper {
         id: physicalKeysMapper
         objectName: "physicalKeysMapper"

plugins/Powerd/Powerd.cpp
@@ -60,6 +60,13 @@ Powerd::Powerd(QObject* parent)
                                       this,
                                       SLOT(handleDisplayPowerStateChange(int, int)));
 
+    lomiriScreen->connection().connect(QStringLiteral("com.canonical.Unity.Screen"),
+                                       QStringLiteral("/com/canonical/Unity/Screen"),
+                                       QStringLiteral("com.canonical.Unity.Screen"),
+                                       QStringLiteral("HighBrightnessModeChange"),
+                                       this,
+                                       SLOT(handleHighBrightnessModeChange(bool)));
+
     systemSettings = g_settings_new("com.lomiri.touch.system");
     g_signal_connect(systemSettings, "changed::auto-brightness", G_CALLBACK(autoBrightnessChanged), lomiriScreen);
     g_signal_connect(systemSettings, "changed::activity-timeout", G_CALLBACK(activityTimeoutChanged), lomiriScreen);
@@ -95,4 +102,18 @@ void Powerd::handleDisplayPowerStateChange(int status, int reason)
         cachedStatus = (Status)status;
         Q_EMIT statusChanged((DisplayStateChangeReason)reason);
     }
+}
+
+void Powerd::handleHighBrightnessModeChange(bool enabled)
+{
+    if (m_highBrightnessModeEnabled == enabled) {
+        return;
+    }
+    m_highBrightnessModeEnabled = enabled;
+    Q_EMIT highBrightnessModeEnabledChanged();
+}
+
+bool Powerd::highBrightnessModeEnabled() const
+{
+    return m_highBrightnessModeEnabled;
 }

plugins/Powerd/Powerd.h
@@ -28,6 +28,7 @@ class Powerd: public QObject
 {
     Q_OBJECT
     Q_PROPERTY(Status status READ status NOTIFY statusChanged)
+    Q_PROPERTY(bool highBrightnessModeEnabled READ highBrightnessModeEnabled NOTIFY highBrightnessModeEnabledChanged FINAL)
 
 public:
     enum DisplayStateChangeReason {
@@ -55,16 +56,21 @@ public:
     // Not exposed via Q_PROPERTY because we need the 'reason' argument too
     Q_INVOKABLE void setStatus(Status status, DisplayStateChangeReason reason);
 
+    bool highBrightnessModeEnabled() const;
+
 Q_SIGNALS:
     void statusChanged(DisplayStateChangeReason reason);
+    void highBrightnessModeEnabledChanged();
 
 private Q_SLOTS:
     void handleDisplayPowerStateChange(int status, int reason);
+    void handleHighBrightnessModeChange(bool enabled);
 
 private:
     QDBusInterface *lomiriScreen;
     GSettings *systemSettings;
     Status cachedStatus;
+    bool m_highBrightnessModeEnabled;
 };
 
 #endif

plugins/Utils/deviceconfig.cpp
@@ -113,3 +113,18 @@ bool DeviceConfig::supportsMultiColorLed() const
 {
     return m_info->contains("supportsMultiColorLed");
 }
+
+quint8 DeviceConfig::sensorLocationX() const
+{
+    return QString::fromStdString(m_info->get("udfpLocationX", "0")).toUInt();
+}
+
+quint8 DeviceConfig::sensorLocationY() const
+{
+    return QString::fromStdString(m_info->get("udfpLocationY", "0")).toUInt();
+}
+
+quint8 DeviceConfig::sensorRadius() const
+{
+    return QString::fromStdString(m_info->get("udfpSensorRadius", "0")).toUInt();
+}

plugins/Utils/deviceconfig.h
@@ -35,6 +35,10 @@ class DeviceConfig: public QObject
     Q_PROPERTY(QString category READ category NOTIFY changed)
     Q_PROPERTY(bool supportsMultiColorLed READ supportsMultiColorLed NOTIFY changed)
 
+    Q_PROPERTY(quint8 sensorLocationX READ sensorLocationX FINAL)
+    Q_PROPERTY(quint8 sensorLocationY READ sensorLocationY FINAL)
+    Q_PROPERTY(quint8 sensorRadius READ sensorRadius FINAL)
+
 public:
     DeviceConfig(QObject *parent = nullptr);
     ~DeviceConfig() = 0;
@@ -49,6 +53,10 @@ public:
     Qt::ScreenOrientation invertedPortraitOrientation() const;
     QString category() const;
     bool supportsMultiColorLed() const;
+
+    quint8 sensorLocationX() const;
+    quint8 sensorLocationY() const;
+    quint8 sensorRadius() const;
 
 // for tests
     Q_INVOKABLE void refresh() { Q_EMIT changed(); }

qml/Components/UDFPSDimmer.qml
@@ -1,0 +1,84 @@
+/*
+ * Copyright 2025-2026 Volla Systeme GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import QtQuick 2.4
+import QtQuick.Window 2.2
+import Lomiri.Components 1.3
+import Biometryd 0.0
+import Utils 0.1
+
+/* Necessary when using high brightness mode (HBM) to avoid blinding the user */
+
+Item {
+    id: udfps
+
+    DeviceConfig {
+        id: deviceConfig
+    }
+
+    visible: Powerd.highBrightnessModeEnabled
+
+    // TODO: read USFPDS position from biometryd
+    property real baseX: deviceConfig.sensorLocationX
+    property real baseY: deviceConfig.sensorLocationY
+    property real sensorRadius: deviceConfig.sensorRadius
+
+    property real sensorLocationX : {
+        switch (Screen.orientation) {
+        case Qt.PortraitOrientation:
+            return baseX
+        case Qt.LandscapeOrientation:
+            return baseY
+        case Qt.InvertedPortraitOrientation:
+            return udfps.width - baseX
+        case Qt.InvertedLandscapeOrientation:
+            return udfps.height - baseY
+        default:
+            return baseX
+        }
+    }
+
+    property real sensorLocationY: {
+        switch (Screen.orientation) {
+        case Qt.PortraitOrientation:
+            return baseY
+        case Qt.LandscapeOrientation:
+            return udfps.width - baseX
+        case Qt.InvertedPortraitOrientation:
+            return udfps.height - baseY
+        case Qt.InvertedLandscapeOrientation:
+            return baseX
+        default:
+            return baseY
+        }
+    }
+
+    Rectangle {
+        anchors.fill: parent
+        color: "black"
+        opacity: 0.9
+    }
+
+    Rectangle {
+        id: indicator
+        width: udfps.sensorRadius * 2
+        height: width
+        radius: width / 2
+        color: "white"
+        x: udfps.sensorLocationX - udfps.sensorRadius
+        y: udfps.sensorLocationY - udfps.sensorRadius
+    }
+}

commit 6aeef279c815705e280eba318507d7671f5313e5

qml/Shell: make sure UDFPSDimmer covers everything
HBM mode will increase brightness for the entire screen, so the dimmer
must cover everything, or there will be parts of the screen which are
still blindingly bright.

Fixes: 463493b237a0 ("Shell: Add USFPS dimmer")

qml/Shell.qml
@@ -267,7 +267,8 @@ StyledItem {
     UDFPSDimmer {
         visible: Powerd.highBrightnessModeEnabled
         anchors.fill: parent
-        z: 9
+        /* Dimmer overlay must cover everything. */
+        z: 100
     }
 
     PhysicalKeysMapper {